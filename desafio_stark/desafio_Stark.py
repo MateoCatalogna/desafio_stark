dcfaaea7cf1bf0e770dc2292b5229a97dd32893c
Mateo <catalognamateo@gmail.com>
2023-10-19 16:47:54 -0300

    stark terminado

--- desafio_stark/desafio_Stark.py
+++ desafio_stark/desafio_Stark.py
@@ -294,4 +294,188 @@ def obtener_dato_de_personaje(lista_personajes):
         else:
             print("Opción no válida. Por favor, seleccione una opción válida (1-16).")
 
-obtener_dato_de_personaje(lista_personajes)
+
+
+
+def tipo_color_ojos(lista_de_personajes):
+    blue_eyes = 0
+    red_eyes = 0
+    brown_eyes = 0
+    yellow_eyes = 0
+    green_eyes = 0
+    silver_eyes = 0
+    hazel_eyes = 0
+    for personaje in lista_de_personajes:
+        color_ojos = personaje["color_ojos"]
+        match color_ojos:
+            case "Blue" | "blue":
+                blue_eyes += 1
+            case  "Red" | "red":
+                red_eyes += 1
+            case "Brown" | "brown":
+                brown_eyes += 1
+            case "Yellow" | "yellow":
+                yellow_eyes += 1
+            case "Green" | "green ":
+                green_eyes += 1
+            case "Silver" | "silver":
+                silver_eyes += 1
+            case "Hazel" | "hazel":
+                hazel_eyes += 1
+            case other:
+                print("Color de ojos no registrado")
+    print(f"hay {blue_eyes} suoerheroes de ojos azules, {red_eyes} superheroes de ojos rojos, {brown_eyes} superheroes de ojos marrones, {yellow_eyes} superheroes de ojos amarillos, {green_eyes} superheroes de ojos verdes, {silver_eyes} superheroes de ojos color silver, {hazel_eyes} superheroes de ojos color hazel")
+
+
+
+def color_de_pelo_superheroes(lista_de_personajes):
+    green_hair = 0
+    red_hair = 0
+    black_hair = 0
+    yellow_hair = 0
+    no_hair = 0
+    blond_hair = 0
+    brown_hair = 0
+    white_hair = 0
+    for personaje in lista_de_personajes:
+        color_de_pelo = personaje["color_pelo"]
+        match color_de_pelo:
+            case "Green" | "green":
+                green_hair += 1
+            case "Red" | "red":
+                red_hair += 1
+            case "Black" | "black":
+                black_hair += 1
+            case "Yellow" | "yellow":
+                yellow_hair += 1
+            case "Blond" | "blond":
+                blond_hair += 1
+            case "Brown" | "brown":
+                brown_hair += 1
+            case "White" | "white":
+                white_hair += 1
+            case other:
+                no_hair += 1
+    print(f"hay {no_hair} superheroes sin pelo, {green_hair} superheores con el pelo verde, {red_hair} superheroes con pelo rojo,{black_hair} superheroes con pelo negro, {yellow_hair} superheroes con pelo amarillo, {blond_hair} superheroes rubios, {brown_hair} superheroes con el pelo marron y {white_hair} superheroes con el pelo blanco")
+    
+
+
+def tipo_inteligencia_superheroes(lista_de_personajes):
+    average_inteligence = 0
+    good_inteligence = 0
+    high_inteligence = 0
+    no_intelligence = 0
+    for personaje in lista_de_personajes:
+        inteligencia = personaje["inteligencia"]
+        match inteligencia:
+            case "good":
+                good_inteligence += 1
+            case "high":
+                high_inteligence += 1
+            case "average":
+                average_inteligence += 1
+            case other:
+                no_intelligence += 1
+
+    print(f"hay {no_intelligence} superheroes sin inteligencia, {good_inteligence} superheroes con inteligencia tipo 'good', {high_inteligence} superheroes con inteligencvia tipo 'high, {average_inteligence} superheroes con inteligencia tipo 'average'")
+
+    
+    
+def lista_segun_color_ojos(lista_de_personajes):
+    blue_eyes = []
+    red_eyes = []
+    brown_eyes = []
+    yellow_eyes = []
+    green_eyes = []
+    silver_eyes = []
+    hazel_eyes = []
+    for personaje in lista_de_personajes:
+        color_ojos = personaje["color_ojos"]
+        match color_ojos:
+            case "Blue" | "blue":
+                blue_eyes.append(personaje["nombre"])
+            case  "Red" | "red":
+                red_eyes.append(personaje["nombre"])
+            case "Brown" | "brown":
+                brown_eyes.append(personaje["nombre"])
+            case "Yellow" | "yellow":
+                yellow_eyes.append(personaje["nombre"])
+            case "Green" | "green":
+                green_eyes.append(personaje["nombre"])
+            case "Silver" | "silver":
+                silver_eyes.append(personaje["nombre"])
+            case "Hazel" | "hazel":
+                hazel_eyes.append(personaje["nombre"])
+            case other:
+                print("Color de ojos no registrado")
+
+    print("Los superhéroes con ojos azules son:", ", ".join(blue_eyes))
+    print("Los superhéroes con ojos rojos son:", ", ".join(red_eyes))
+    print("Los superhéroes con ojos marrones son:", ", ".join(brown_eyes))
+    print("Los superhéroes con ojos amarillos son:", ", ".join(yellow_eyes))
+    print("Los superhéroes con ojos verdes son:", ", ".join(green_eyes))
+    print("Los superhéroes con ojos plateados son:", ", ".join(silver_eyes))
+    print("Los superhéroes con ojos avellana son:", ", ".join(hazel_eyes))
+    
+
+def lista_segun_color_pelo(lista_de_personajes):
+    green_hair = []
+    red_hair = []
+    black_hair = []
+    yellow_hair = []
+    no_hair = []
+    blond_hair = []
+    brown_hair = []
+    white_hair = []
+    for personaje in lista_de_personajes:
+        color_de_pelo = personaje["color_pelo"]
+        match color_de_pelo:
+            case "Green" | "green":
+                green_hair.append(personaje["nombre"])
+            case "Red" | "red":
+                red_hair.append(personaje["nombre"])
+            case "Black" | "black":
+                black_hair.append(personaje["nombre"])
+            case "Yellow" | "yellow":
+                yellow_hair.append(personaje["nombre"])
+            case "Blond" | "blond":
+                blond_hair.append(personaje["nombre"])
+            case "Brown" | "brown":
+                brown_hair.append(personaje["nombre"])
+            case "White" | "white":
+                white_hair.append(personaje["nombre"])
+            case other:
+                no_hair.append(personaje["nombre"])
+
+    print("Los superhéroes con pelo verde son:", ", ".join(green_hair))
+    print("Los superhéroes con pelo rojo son:", ", ".join(red_hair))
+    print("Los superhéroes con pelo marron son:", ", ".join(brown_hair))
+    print("Los superhéroes con pelo amarillo son:", ", ".join(yellow_hair))
+    print("Los superhéroes rubios son:", ", ".join(blond_hair))
+    print("Los superhéroes con pelo blanc son:", ", ".join(white_hair))
+    print("Los superhéroes sin pelo son:", ", ".join(no_hair))
+
+
+def listar_segun_inteligencia(lista_de_personajes):
+    average_inteligence = []
+    good_inteligence = []
+    high_inteligence = []
+    no_intelligence = []
+    for personaje in lista_de_personajes:
+        inteligencia = personaje["inteligencia"]
+        match inteligencia:
+            case "good":
+                good_inteligence.append(personaje["nombre"])
+            case "high":
+                high_inteligence.append(personaje["nombre"])
+            case "average":
+                average_inteligence.append(personaje["nombre"])
+            case other:
+                no_intelligence.append(personaje["nombre"])
+    
+    print("Los superhéroes con inteligencia tipo 'good' son:", ", ".join(good_inteligence))
+    print("Los superhéroes con inteligencia tipo 'high' son:", ", ".join(high_inteligence))
+    print("Los superhéroes con inteligencia tipo 'average' son:", ", ".join(average_inteligence))
+    print("Los superhéroes sin inteligencia son:", ", ".join(no_intelligence))
+
+listar_segun_inteligencia(lista_personajes)
